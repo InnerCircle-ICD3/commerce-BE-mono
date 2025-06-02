3e0ff990ca5ba1f4dfab25b2713b23acca26800e
UjiinEatingTangerines <1996.devjiinu@gmail.com>
2025-06-02 19:31:10 +0900

    refactor: Auth error code 수정

--- src/main/kotlin/com/fastcampus/commerce/common/error/AuthErrorCode.kt
+++ src/main/kotlin/com/fastcampus/commerce/common/error/AuthErrorCode.kt
@@ -5,7 +5,7 @@ enum class AuthErrorCode(
     override val message: String,
     override val logLevel: LogLevel,
 ) : ErrorCode {
-    INVALID_TOKEN("AUTH-001", "Invalid token", LogLevel.WARN),
-    EXPIRED_TOKEN("AUTH-002", "Expired token", LogLevel.WARN),
-    TOKEN_NOT_FOUND("AUTH-003", "Token not found", LogLevel.WARN),
+    INVALID_TOKEN("ATH-001", "Invalid token", LogLevel.WARN),
+    EXPIRED_TOKEN("ATH-002", "Expired token", LogLevel.WARN),
+    TOKEN_NOT_FOUND("ATH-003", "Token not found", LogLevel.WARN),
 }
