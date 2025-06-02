9ef64efbdf8f3f5d541ea710e4507c8890befd01
dhlee3994 <dhlee3994@gmail.com>
2025-06-02 20:36:14 +0900

    fix: 오류 코드 중복 및 메시지 수정

--- src/main/kotlin/com/fastcampus/commerce/common/error/CommonErrorCode.kt
+++ src/main/kotlin/com/fastcampus/commerce/common/error/CommonErrorCode.kt
@@ -5,7 +5,7 @@ enum class CommonErrorCode(
     override val message: String,
     override val logLevel: LogLevel,
 ) : ErrorCode {
-    SERVER_ERROR("SYS-001", "An unexpected error has occurred.", LogLevel.ERROR),
+    SERVER_ERROR("SYS-001", "시스템 오류가 발생했습니다.", LogLevel.ERROR),
 
-    FIELD_ERROR("FED-001", "An unexpected error has occurred.", LogLevel.WARN),
+    FIELD_ERROR("FED-001", "올바르지 않은 요청입니다.", LogLevel.WARN),
 }
