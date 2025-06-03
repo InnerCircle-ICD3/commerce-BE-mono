d2276c541ee2acb2240f5284bd7b2782e6c9a266
dhlee3994 <dhlee3994@gmail.com>
2025-06-03 20:04:40 +0900

    fix: RegisterProductApiRequest 필드 순서 수정

--- src/main/kotlin/com/fastcampus/commerce/admin/product/interfaces/request/RegisterProductApiRequest.kt
+++ src/main/kotlin/com/fastcampus/commerce/admin/product/interfaces/request/RegisterProductApiRequest.kt
@@ -14,9 +14,9 @@ data class RegisterProductApiRequest(
     @field:PositiveOrZero(message = "재고는 음수일 수 없습니다.")
     val quantity: Int,
     @field:NotBlank(message = "썸네일이 비어있습니다.")
-    val detailImage: String,
-    @field:NotBlank(message = "상품 상세이미지가 비어있습니다.")
     val thumbnail: String,
+    @field:NotBlank(message = "상품 상세이미지가 비어있습니다.")
+    val detailImage: String,
     @field:NotNull(message = "강도 카테고리 아이디가 비어있습니다.")
     val intensityId: Long,
     @field:NotNull(message = "컵사이즈 카테고리 아이디가 비어있습니다.")
