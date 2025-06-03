5cd7aa866763fa99ebadabffb46522a8ec3ebecd
UjiinEatingTangerines <1996.devjiinu@gmail.com>
2025-06-03 16:31:15 +0900

    WIP: rebase 전 임시 커밋

--- src/main/kotlin/com/fastcampus/commerce/auth/infrastructure/security/oauth/config/OAuth2PropertyConfig.kt
+++
@@ -1,8 +0,0 @@
-package com.fastcampus.commerce.auth.infrastructure.security.oauth.config
-
-import org.springframework.boot.context.properties.EnableConfigurationProperties
-import org.springframework.context.annotation.Configuration
-
-@Configuration
-@EnableConfigurationProperties(NaverOAuth2Properties::class)
-class OAuth2PropertyConfig
