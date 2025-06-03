9861e9cb776969cd3c383bbdc832c21d13007e29
CHOSSEANG <jng00153@gmail.com>
2025-06-04 00:38:19 +0900

    fix(product) : findById -> findByProductId 로 수정

--- src/main/kotlin/com/fastcampus/commerce/product/infrastructure/InventoryJpaRepository.kt
+++ src/main/kotlin/com/fastcampus/commerce/product/infrastructure/InventoryJpaRepository.kt
@@ -2,5 +2,8 @@ package com.fastcampus.commerce.product.infrastructure
 
 import com.fastcampus.commerce.product.domain.entity.Inventory
 import org.springframework.data.jpa.repository.JpaRepository
+import java.util.Optional
 
-interface InventoryJpaRepository : JpaRepository<Inventory, Long>
+interface InventoryJpaRepository : JpaRepository<Inventory, Long>{
+    fun findByProductId(productId: Long): Optional<Inventory>
+}
