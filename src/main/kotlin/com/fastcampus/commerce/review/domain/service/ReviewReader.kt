753b469920d20bc48e813ca8fcf97d4bb155a0d5
dhlee3994 <dhlee3994@gmail.com>
2025-06-04 23:19:06 +0900

    feat: 리뷰 조회 기능 추가

--- src/main/kotlin/com/fastcampus/commerce/review/domain/service/ReviewReader.kt
+++ src/main/kotlin/com/fastcampus/commerce/review/domain/service/ReviewReader.kt
@@ -1,5 +1,8 @@
 package com.fastcampus.commerce.review.domain.service
 
+import com.fastcampus.commerce.common.error.CoreException
+import com.fastcampus.commerce.review.domain.entity.Review
+import com.fastcampus.commerce.review.domain.error.ReviewErrorCode
 import com.fastcampus.commerce.review.domain.repository.ReviewRepository
 import org.springframework.stereotype.Component
 
@@ -9,5 +12,10 @@ class ReviewReader(
 ) {
     fun existsByUserIdAndOrderItemId(userId: Long, orderItemId: Long): Boolean {
         return reviewRepository.existsByUserIdAndOrderItemId(userId, orderItemId)
+    }
+
+    fun getReviewById(reviewId: Long): Review {
+        return reviewRepository.findById(reviewId)
+            .orElseThrow { throw CoreException(ReviewErrorCode.REVIEW_NOT_FOUND) }
     }
 }
