72ab7725df1e8d5a182b90dc8908212854d976f1
UjiinEatingTangerines <1996.devjiinu@gmail.com>
2025-06-04 15:01:12 +0900

    feat(auth): Auth 버저닝 제거

--- src/main/kotlin/com/fastcampus/commerce/auth/api/controller/AuthController.kt
+++ src/main/kotlin/com/fastcampus/commerce/auth/api/controller/AuthController.kt
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/api/v1/auth")
+@RequestMapping("/auth")
 class AuthController(
     private val authService: AuthService,
 ) {
