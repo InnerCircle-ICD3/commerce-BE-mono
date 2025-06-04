be95c4580d3b5d0f75781b276808abca95a95d9c
dhlee3994 <121185072+dhlee3994@users.noreply.github.com>
2025-06-04 21:32:56 +0900

    fix: `@Query` 누락 수정 (#39)

--- src/main/kotlin/com/fastcampus/commerce/product/infrastructure/InventoryJpaRepository.kt
+++ src/main/kotlin/com/fastcampus/commerce/product/infrastructure/InventoryJpaRepository.kt
@@ -3,6 +3,7 @@ package com.fastcampus.commerce.product.infrastructure
 import com.fastcampus.commerce.product.domain.entity.Inventory
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Lock
+import org.springframework.data.jpa.repository.Query
 import java.util.Optional
 import jakarta.persistence.LockModeType
 
@@ -10,5 +11,6 @@ interface InventoryJpaRepository : JpaRepository<Inventory, Long> {
     fun findByProductId(productId: Long): Optional<Inventory>
 
     @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("select c from Inventory c where c.productId = :productId")
     fun findByProductIdForUpdate(productId: Long): Optional<Inventory>
 }
