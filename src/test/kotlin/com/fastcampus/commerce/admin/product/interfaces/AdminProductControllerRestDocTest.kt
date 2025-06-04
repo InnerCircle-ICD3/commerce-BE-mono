3f82a4e600eb6068567a5ab1fbd16617994a307c
dhlee3994 <dhlee3994@gmail.com>
2025-06-04 22:09:05 +0900

    test: RestDocs 테스트 클래스명 변경

--- src/test/kotlin/com/fastcampus/commerce/admin/product/interfaces/AdminProductControllerRestDocTest.kt
+++ src/test/kotlin/com/fastcampus/commerce/admin/product/interfaces/AdminProductControllerRestDocTest.kt
@@ -24,7 +24,7 @@ import org.springframework.test.web.servlet.MockMvc
 @AutoConfigureRestDocs
 @AutoConfigureMockMvc
 @WebMvcTest(AdminProductController::class)
-class AdminProductControllerTest : DescribeSpec() {
+class AdminProductControllerRestDocTest : DescribeSpec() {
     override fun extensions() = listOf(SpringExtension)
 
     @Autowired
