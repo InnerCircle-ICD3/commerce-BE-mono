f00b221ed1fa843d13e93fdd6c3d00d439d08c20
CHOSSEANG <jng00153@gmail.com>
2025-06-07 15:19:36 +0900

    feat(cart) : AuthenticationPrincipal 제거

--- src/main/kotlin/com/fastcampus/commerce/cart/interfaces/CartItemController.kt
+++ src/main/kotlin/com/fastcampus/commerce/cart/interfaces/CartItemController.kt
@@ -2,12 +2,9 @@ package com.fastcampus.commerce.cart.interfaces
 
 import com.fastcampus.commerce.cart.application.CartItemService
 import org.springframework.web.bind.annotation.GetMapping
-//import com.fastcampus.commerce.user.domain.entity.User
-//import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -16,26 +13,27 @@ class CartItemController(
 ) {
     @GetMapping("/carts")
     fun getCarts(
-//        @AuthenticationPrincipal user: User,
     ): CartRetrievesResponse {
-        return cartItemService.getCarts(1L)
+        val userId = 1L
+        return cartItemService.getCarts(userId)
     }
 
     @PostMapping("/cart/items")
     fun addToCart(
         @RequestBody request: CartCreateRequest,
-//        @AuthenticationPrincipal user: User
     ): CartCreateResponse {
-        val response = cartItemService.addToCart(1L, request.productId, request.quantity)
+        val userId = 1L
+        val response = cartItemService.addToCart(userId, request.productId, request.quantity)
         return response
     }
 
     @PatchMapping("/cart/items")
     fun updateCartItem(
-//        @AuthenticationPrincipal user: User,
         @RequestBody request: CartUpdateRequest,
-    ): CartUpdateResponse {
-        val cartResponse = cartItemService.updateCartItem(1L,request)
+    ): CartUpdateResponse
+    {
+        val userId = 1L
+        val cartResponse = cartItemService.updateCartItem(userId,request)
         return cartResponse
     }
 }
