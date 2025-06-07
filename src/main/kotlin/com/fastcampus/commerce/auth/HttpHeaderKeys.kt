d80f911264d9c11d7ab965ca28791c0704b4df08
CHOSSEANG <jng00153@gmail.com>
2025-06-07 18:35:45 +0900

    feat(release) : auth, user 삭제

--- src/main/kotlin/com/fastcampus/commerce/auth/HttpHeaderKeys.kt
+++
@@ -1,8 +0,0 @@
-package com.fastcampus.commerce.auth
-
-class HttpHeaderKeys {
-    companion object {
-        const val USER_ID = "x-commerce-user-id"
-        const val ACCESS_TOKEN = "x-commerce-access-token"
-    }
-}
--- src/main/kotlin/com/fastcampus/commerce/user/api/service/UserService.kt
+++
@@ -1,6 +0,0 @@
-package com.fastcampus.commerce.user.api.service
-
-import org.springframework.stereotype.Service
-
-@Service
-class UserService()
