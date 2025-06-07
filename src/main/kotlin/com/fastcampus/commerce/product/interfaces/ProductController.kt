d86e229383ea1d3cdfea3880c20f2ee38054c1c8
dhlee3994 <dhlee3994@gmail.com>
2025-06-07 15:13:05 +0900

    feat: 상품 검색 API 추가

---
+++ src/main/kotlin/com/fastcampus/commerce/product/interfaces/ProductController.kt
@@ -0,0 +1,27 @@
+package com.fastcampus.commerce.product.interfaces
+
+import com.fastcampus.commerce.common.response.PagedData
+import com.fastcampus.commerce.product.application.ProductQueryService
+import com.fastcampus.commerce.product.interfaces.request.SearchProductApiRequest
+import com.fastcampus.commerce.product.interfaces.response.SearchProductApiResponse
+import org.springframework.data.domain.Pageable
+import org.springframework.data.web.PageableDefault
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ModelAttribute
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RequestMapping("/products")
+@RestController
+class ProductController(
+    private val productQueryService: ProductQueryService,
+) {
+    @GetMapping
+    fun searchProducts(
+        @ModelAttribute request: SearchProductApiRequest,
+        @PageableDefault pageable: Pageable,
+    ): PagedData<SearchProductApiResponse> {
+        val products = productQueryService.getProducts(request.toServiceRequest(), pageable)
+        return PagedData.of(products.map(SearchProductApiResponse::from))
+    }
+}
