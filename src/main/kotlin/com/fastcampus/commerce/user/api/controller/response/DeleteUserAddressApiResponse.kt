4f3c9aca1899f91e9c352b0077f6aba48bfd5d23
CHOSSEANG <jng00153@gmail.com>
2025-06-09 20:47:08 +0900

    feat(release): User 추가 삭제

--- src/main/kotlin/com/fastcampus/commerce/user/api/controller/response/DeleteUserAddressApiResponse.kt
+++
@@ -1,5 +0,0 @@
-package com.fastcampus.commerce.user.api.controller.response
-
-data class DeleteUserAddressApiResponse(
-    val message: String = "OK",
-)
--- src/main/kotlin/com/fastcampus/commerce/user/api/controller/response/UpdateUserAddressApiResponse.kt
+++
@@ -1,5 +0,0 @@
-package com.fastcampus.commerce.user.api.controller.response
-
-data class UpdateUserAddressApiResponse(
-    val addressId: Long,
-)
