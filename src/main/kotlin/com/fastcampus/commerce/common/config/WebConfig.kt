8250e3aed5a3604a758404064d3f7d7791dbeb9b
dhlee3994 <dhlee3994@gmail.com>
2025-06-12 00:52:09 +0900

    feat: CustomPageable 추가

--- src/main/kotlin/com/fastcampus/commerce/common/config/WebConfig.kt
+++ src/main/kotlin/com/fastcampus/commerce/common/config/WebConfig.kt
@@ -1,14 +1,22 @@
 package com.fastcampus.commerce.common.config
 
+import com.fastcampus.commerce.common.resolver.CustomPageableArgumentResolver
 import org.springframework.context.annotation.Configuration
+import org.springframework.web.method.support.HandlerMethodArgumentResolver
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
-class WebConfig : WebMvcConfigurer {
+class WebConfig(
+    private val customPageableArgumentResolver: CustomPageableArgumentResolver,
+) : WebMvcConfigurer {
     override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
         registry.addResourceHandler("/api/**")
             .addResourceLocations("file:./docs/api/")
             .setCachePeriod(0)
+    }
+
+    override fun addArgumentResolvers(resolvers: MutableList<HandlerMethodArgumentResolver?>) {
+        resolvers.add(customPageableArgumentResolver)
     }
 }
