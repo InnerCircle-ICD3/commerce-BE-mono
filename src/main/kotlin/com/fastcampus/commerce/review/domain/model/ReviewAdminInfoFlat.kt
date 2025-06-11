6c0c25ab3c2e5f4d6dd9d3af51cc57d2197ddcce
dhlee3994 <dhlee3994@gmail.com>
2025-06-12 00:53:50 +0900

    feat: 관리자 리뷰 검색 데이터 레이어 구현

---
+++ src/main/kotlin/com/fastcampus/commerce/review/domain/model/ReviewAdminInfoFlat.kt
@@ -0,0 +1,19 @@
+package com.fastcampus.commerce.review.domain.model
+
+import com.querydsl.core.annotations.QueryProjection
+import java.time.LocalDateTime
+
+data class ReviewAdminInfoFlat
+    @QueryProjection
+    constructor(
+        val reviewId: Long,
+        val rating: Int,
+        val content: String,
+        val adminReplyContent: String? = null,
+        val adminReplyCreatedAt: LocalDateTime? = null,
+        val userId: Long,
+        val userNickname: String,
+        val productId: Long,
+        val productName: String,
+        val createdAt: LocalDateTime,
+    )
