ebd21c6316e664d346286fceca83862cf085f485
UjiinEatingTangerines <1996.devjiinu@gmail.com>
2025-06-14 03:37:16 +0900

    feat(cart): cartItem 관련 dto, reader, reader 구현체 구현

---
+++ src/main/kotlin/com/fastcampus/commerce/cart/application/query/dto/CartItemDto.kt
@@ -0,0 +1,25 @@
+package com.fastcampus.commerce.cart.application.query.dto
+
+import com.fastcampus.commerce.cart.domain.entity.CartItem
+
+data class CartItemDto(
+    val id: Long?=null,
+    val productId: Long,
+    val productName: String?=null,
+    val quantity: Int,
+    val unitPrice: Int?=null,
+    val productSnapshotId: Long?=null,
+) {
+    companion object {
+        fun from(cartItem: CartItem): CartItemDto {
+            return CartItemDto(
+                id = cartItem.id,
+                productId = cartItem.productId,
+                productName = null,
+                quantity = cartItem.quantity,
+                unitPrice = null,
+                productSnapshotId = null,
+            )
+        }
+    }
+}
---
+++ src/main/kotlin/com/fastcampus/commerce/cart/infrastructure/query/CartItemReaderImpl.kt
@@ -0,0 +1,32 @@
+package com.fastcampus.commerce.cart.infrastructure.query
+
+import com.fastcampus.commerce.cart.application.query.CartItemReader
+import com.fastcampus.commerce.cart.application.query.dto.CartItemDto
+import com.fastcampus.commerce.cart.infrastructure.repository.CartItemRepository
+import com.fastcampus.commerce.order.application.query.ProductSnapshotReader
+import com.fastcampus.commerce.product.domain.service.ProductReader
+import org.springframework.stereotype.Component
+
+@Component
+class CartItemReaderImpl(
+    private val cartItemRepository: CartItemRepository,
+    private val productReader: ProductReader,
+    private val productSnapshotReader: ProductSnapshotReader
+) : CartItemReader {
+
+    override fun readCartItems(userId: Long, cartItemIds: Set<Long>): List<CartItemDto> {
+        val cartItems = cartItemRepository.findAllByUserId(userId)!!.filter { cartItemIds.contains(it.id) }
+        return cartItems.map { cartItem ->
+            val product = productReader.getProductById(cartItem.productId)
+            val productSnapshot = productSnapshotReader.getById(product.id!!)
+            CartItemDto(
+                id = cartItem.id,
+                productId = cartItem.productId,
+                productName = product.name,
+                quantity = cartItem.quantity,
+                unitPrice = product.price,
+                productSnapshotId = productSnapshot.id,
+            )
+        }
+    }
+}
