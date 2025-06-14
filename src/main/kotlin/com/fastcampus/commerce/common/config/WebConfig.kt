f9016ee29c3332c89ea6587e26889d735a1ceea6
UjiinEatingTangerines <1996.devjiinu@gmail.com>
2025-06-14 21:56:12 +0900

    feat(common): 동현님 작업물 원복

--- src/main/kotlin/com/fastcampus/commerce/common/config/WebConfig.kt
+++ src/main/kotlin/com/fastcampus/commerce/common/config/WebConfig.kt
@@ -1,6 +1,7 @@
 package com.fastcampus.commerce.common.config
 
 import com.fastcampus.commerce.auth.interfaces.web.security.resolver.RoleBasedUserArgumentResolver
+import com.fastcampus.commerce.common.resolver.CustomPageableArgumentResolver
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.method.support.HandlerMethodArgumentResolver
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
@@ -8,6 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
 class WebConfig(
+    private val customPageableArgumentResolver: CustomPageableArgumentResolver,
     private val roleBasedUserArgumentResolver: RoleBasedUserArgumentResolver
 ) : WebMvcConfigurer {
     override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
@@ -18,6 +20,7 @@ class WebConfig(
     }
 
     override fun addArgumentResolvers(resolvers: MutableList<HandlerMethodArgumentResolver>) {
+        resolvers.add(customPageableArgumentResolver)
         resolvers.add(roleBasedUserArgumentResolver)
     }
 }
