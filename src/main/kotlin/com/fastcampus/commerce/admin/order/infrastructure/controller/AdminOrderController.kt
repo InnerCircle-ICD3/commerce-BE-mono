6508da6ba087807bb0904bc4e0e62d387e20c529
UjiinEatingTangerines <1996.devjiinu@gmail.com>
2025-06-17 02:20:55 +0900

    feat(admin(order)): 어드민 내 주문 조회, 주문 상세 조회, 주문 요청, 주문 취소, 주문 수정 기능 구현

--- src/main/kotlin/com/fastcampus/commerce/admin/order/infrastructure/controller/AdminOrderController.kt
+++ src/main/kotlin/com/fastcampus/commerce/admin/order/infrastructure/controller/AdminOrderController.kt
@@ -1,14 +1,23 @@
 package com.fastcampus.commerce.admin.order.infrastructure.controller
 
 import com.fastcampus.commerce.admin.order.application.AdminOrderService
+import com.fastcampus.commerce.admin.order.infrastructure.request.AdminOrderCreateRequest
 import com.fastcampus.commerce.admin.order.infrastructure.request.AdminOrderSearchRequest
+import com.fastcampus.commerce.admin.order.infrastructure.request.AdminOrderUpdateRequest
+import com.fastcampus.commerce.admin.order.infrastructure.response.AdminOrderCreateResponse
+import com.fastcampus.commerce.admin.order.infrastructure.response.AdminOrderDetailResponse
 import com.fastcampus.commerce.admin.order.infrastructure.response.AdminOrderListResponse
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.domain.Sort
 import org.springframework.data.web.PageableDefault
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.ModelAttribute
+import org.springframework.web.bind.annotation.PatchMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -17,11 +26,39 @@ import org.springframework.web.bind.annotation.RestController
 class AdminOrderController(
     private val adminOrderService: AdminOrderService
 ) {
+    // 주문 조회
     @GetMapping
     fun getOrders(
         @ModelAttribute search: AdminOrderSearchRequest,
         @PageableDefault(size = 20, sort = ["orderDate"], direction = Sort.Direction.DESC) pageable: Pageable
     ): Page<AdminOrderListResponse> {
         return adminOrderService.getOrders(search, pageable)
+    }
+
+    //주문 상세 조회
+    @GetMapping("/{orderId}")
+    fun getOrderDetail(@PathVariable orderId: Long): AdminOrderDetailResponse {
+        return adminOrderService.getOrderDetail(orderId)
+    }
+
+    // 주문 생성
+    @PostMapping
+    fun createOrder(@RequestBody request: AdminOrderCreateRequest): AdminOrderCreateResponse {
+        return adminOrderService.createOrder(request)
+    }
+
+    //주문 취소
+    @DeleteMapping("/{orderId}/cancel")
+    fun cancelOrder(@PathVariable orderId: Long) {
+        adminOrderService.cancelOrder(orderId)
+    }
+
+    //주문 수정
+    @PatchMapping("/{orderId}")
+    fun updateOrder(
+        @PathVariable orderId: Long,
+        @RequestBody request: AdminOrderUpdateRequest
+    ) {
+        adminOrderService.updateOrder(orderId, request)
     }
 }
--- src/main/kotlin/com/fastcampus/commerce/order/domain/error/OrderErrorCode.kt
+++ src/main/kotlin/com/fastcampus/commerce/order/domain/error/OrderErrorCode.kt
@@ -17,4 +17,5 @@ enum class OrderErrorCode(
     UNAUTHORIZED_ORDER_REFUND("ORD-104", "다른 사람의 결제를 환불할 수 없습니다.", LogLevel.WARN),
     NOT_REFUND_REQUESTED_APPROVE("ORD-104", "환불요청한 주문만 환불가능합니다.", LogLevel.WARN),
     NOT_REFUND_REQUESTED_REJECT("ORD-105", "환불요청한 주문만 환불거절 가능합니다.", LogLevel.WARN),
+    ORDER_CANNOT_BE_CANCELLED("ORD-106", "해당 주문은 관리자에 의해 취소할 수 없는 상태입니다.", LogLevel.WARN),
 }
