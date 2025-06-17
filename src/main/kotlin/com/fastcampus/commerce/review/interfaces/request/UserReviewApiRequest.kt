2e0b3ac949f7a753d575b87543a6986b848033ea
dhlee3994 <121185072+dhlee3994@users.noreply.github.com>
2025-06-17 21:53:11 +0900

    fix: 내 리뷰 조회 Query String key 수정 (#108)

--- src/main/kotlin/com/fastcampus/commerce/review/interfaces/request/UserReviewApiRequest.kt
+++ src/main/kotlin/com/fastcampus/commerce/review/interfaces/request/UserReviewApiRequest.kt
@@ -3,7 +3,7 @@ package com.fastcampus.commerce.review.interfaces.request
 import com.fastcampus.commerce.review.application.request.UserReviewRequest
 
 data class UserReviewApiRequest(
-    val period: Int? = null,
+    val monthRange: Int? = null,
 ) {
-    fun toServiceRequest(userId: Long) = UserReviewRequest(userId, period)
+    fun toServiceRequest(userId: Long) = UserReviewRequest(userId, monthRange)
 }
