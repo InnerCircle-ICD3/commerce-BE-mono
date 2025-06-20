10f970469fb67bbe7a6c04396a93409209358f57
dhlee3994 <dhlee3994@gmail.com>
2025-06-20 23:34:29 +0900

    fix: 결제 조회시 WAITING, PAID 상태만 조회 하도록 수정

--- src/main/kotlin/com/fastcampus/commerce/payment/infrastructure/PaymentJpaRepository.kt
+++ src/main/kotlin/com/fastcampus/commerce/payment/infrastructure/PaymentJpaRepository.kt
@@ -2,9 +2,11 @@ package com.fastcampus.commerce.payment.infrastructure
 
 import com.fastcampus.commerce.payment.domain.entity.Payment
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import java.util.Optional
 
 interface PaymentJpaRepository : JpaRepository<Payment, Long> {
+    @Query("SELECT p FROM Payment p WHERE p.orderId = :orderId and p.status in ('WAITING', 'PAID')")
     fun findByOrderId(orderId: Long): Optional<Payment>
 
     fun findByPaymentNumber(paymentNumber: String): Optional<Payment>
