2a2de75bb2c757a7d06553a32c45f88dde2fce23
dhlee3994 <dhlee3994@gmail.com>
2025-06-21 12:48:38 +0900

    hotfix: 리뷰 등록 요청시 validation 숮어

--- src/main/kotlin/com/fastcampus/commerce/review/interfaces/request/RegisterReviewApiRequest.kt
+++ src/main/kotlin/com/fastcampus/commerce/review/interfaces/request/RegisterReviewApiRequest.kt
@@ -5,11 +5,12 @@ import jakarta.validation.constraints.Max
 import jakarta.validation.constraints.Min
 import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.NotNull
+import jakarta.validation.constraints.Positive
 
 data class RegisterReviewApiRequest(
     @field:NotBlank(message = "주문번호가 누락되었습니다.")
     val orderNumber: String,
-    @field:NotNull(message = "주문 항목이 누락되었습니다.")
+    @field:Positive(message = "주문 항목이 누락되었습니다.")
     val orderItemId: Long,
     @field:Min(value = 1, message = "별점은 1~5점 사이로 선택해주세요.")
     @field:Max(value = 5, message = "별점은 1~5점 사이로 선택해주세요.")
