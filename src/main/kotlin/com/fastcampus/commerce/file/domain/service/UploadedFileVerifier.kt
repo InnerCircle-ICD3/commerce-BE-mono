7f0e01b7f74bc4cf292385a75fd14962d843b016
dhlee3994 <dhlee3994@gmail.com>
2025-06-23 22:42:24 +0900

    fix: 리뷰 작성 가능 조건 수정

--- src/main/kotlin/com/fastcampus/commerce/file/domain/service/UploadedFileVerifier.kt
+++ src/main/kotlin/com/fastcampus/commerce/file/domain/service/UploadedFileVerifier.kt
@@ -2,6 +2,7 @@ package com.fastcampus.commerce.file.domain.service
 
 import com.fastcampus.commerce.file.domain.model.UploadUrl
 import com.fastcampus.commerce.file.infrastructure.s3.S3FileReader
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 
 @Component
@@ -9,15 +10,18 @@ class UploadedFileVerifier(
     private val fileReader: FileReader,
     private val s3FileReader: S3FileReader,
 ) {
+    private val log = LoggerFactory.getLogger(javaClass)
+
     fun verifyFileWithS3Urls(fileUrls: List<String>) {
         fileUrls.forEach { verifyFileFromUploadedUrl(UploadUrl(it)) }
     }
 
     private fun verifyFileFromUploadedUrl(uploadUrl: UploadUrl) {
+        log.info("verifyFileFromUploadedUrl: $uploadUrl")
         val s3Key = uploadUrl.storedPath
         val metadata = fileReader.getFileMetadataByStoredPath(s3Key)
         val actualFile = s3FileReader.read(s3Key)
-
+        log.info("actualFile: $actualFile")
         actualFile.verify(metadata)
     }
 }
